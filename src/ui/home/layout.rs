4571fcbf95f620e4484afd8f8417346f61b5cd31
往昔流年 <54844110+zhangsan-feng@users.noreply.github.com>
2024-12-17 17:02:55 +0800

    Update layout.rs

--- src/ui/home/layout.rs
+++ src/ui/home/layout.rs
@@ -34,7 +34,6 @@ impl ApplicationComponent {
     }
 
     pub fn main_home(&mut self, ui:&mut egui::Ui){
-
         egui::CentralPanel::default().show_inside(ui, |ui|{
             match self.current_page {
                 ApplicationPage::HomePage =>{
@@ -69,17 +68,15 @@ impl eframe::App for ApplicationComponent {
             // ui.vertical()   纵
 
 
-
             ui.vertical(|ui| {
 
-                let widget_rect = egui::Rect::from_center_size(Pos2::new(100.0,100.0), Vec2::new(100.0,30.0));
                 let rect = ui.allocate_exact_size(Vec2::new(100.0,30.0).into(),Sense::hover());
 
                 ui.child_ui( rect.0, Layout::centered_and_justified(egui::Direction::BottomUp), None )
                     .selectable_value(&mut self.current_page, ApplicationPage::HomePage, "HomePage");
 
 
-                let rect = ui.allocate_exact_size(Vec2::new(100.0,30.0).into(),Sense::hover());
+                let rect = ui.allocate_exact_size(Vec2::new(100.20,30.0).into(),Sense::hover());
                 ui.child_ui( rect.0, Layout::centered_and_justified(egui::Direction::TopDown), None )
                     .selectable_value(&mut self.current_page, ApplicationPage::TestPage, "TestPage");
 
@@ -87,7 +84,6 @@ impl eframe::App for ApplicationComponent {
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // egui::Window::new("").vscroll(true).show(ctx,|ui|{ctx.settings_ui(ui)});
             self.main_home(ui)
         });
     }
